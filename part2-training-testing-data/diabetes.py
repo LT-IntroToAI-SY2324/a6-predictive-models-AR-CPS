b9e9d9e94b7215d94065a44a09b079db017e77ce
AR-CPS <aaramos4@cps.edu>
2023-11-30 11:23:00 -0600

    changed to berg version

--- part2-training-testing-data/diabetes.py
+++ part2-training-testing-data/diabetes.py
@@ -1,32 +1,21 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_diabetes
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-'''
-**********CREATE THE MODEL**********
-'''
+data = load_diabetes(as_frame=True)
+y = data.target.values
+data = data.frame
+# print(data)
+x = data["bmi"].values
 
-# imports the data and sets x and y values
-data = pd.read_csv("part2-training-testing-data/chirping_data.csv")
-x = data["Temp"].values
-y = data["Chirps"].values
+x = x.reshape(-1, 1)
 
-# separates the data into training and testing sets
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
-print(f"x: {x}")
-print(f"y: {y}")
-print(f"xtrain: {xtrain}")
-print(f"ytrain: {ytrain}")
-print(f"xtest: {xtest}")
-print(f"ytest: {ytest}")
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .10)
 
-
-# reshape the xtrain data into a 2D array
-xtrain = xtrain.reshape(-1, 1)
-
-# create the linear regression model using the training data
+# Create the model
 model = LinearRegression().fit(xtrain, ytrain)
 
 # get the coef_, intercept_ valuesm and r^2 values
@@ -43,13 +32,10 @@ print("R Squared value:", r_squared)
 **********TEST THE MODEL**********
 '''
 
-# reshape the xtest data into a 2D array
-xtest = xtest.reshape(-1,1)
 # get the predicted y values for the xtest values - returns an array of the results
 predict = model.predict(xtest)
 # round the value in the np array to 2 decimal places
 predict = np.around(predict, 2)
-print(predict)
 
 # compare the actual and predicted values
 print("\nTesting Linear Model with Testing Data:")
@@ -59,22 +45,19 @@ for index in range(len(xtest)):
     x_coord = xtest[index] # gets the x value from the xtest dataset
     print("x value:", float(x_coord), "Predicted y value:", predicted_y, "Actual y value:", actual)
 
-'''
-**********CREATE A VISUAL OF THE RESULTS**********
-'''
-#sets the size of the graph
-plt.figure(figsize=(5,4))
+# Graph the data
+# sets the size of the graph
+plt.figure(figsize=(6, 4))
 
-#creates a scatter plot and labels the axes
-plt.scatter(xtrain,ytrain, c="purple", label="Training Data")
-plt.scatter(xtest, ytest, c="blue", label="Testing Data")
+# creates a scatter plot and labels the axes
+plt.scatter(xtrain,ytrain, c="blue")
+plt.scatter(xtest, ytest, c="red")
+plt.plot(xtrain, coef*xtrain + intercept, c="r", label="Line of Best Fit")
 
-plt.scatter(xtest, predict, c="red", label="Predictions")
+plt.xlabel("bmi")
+plt.ylabel("quantitative measure")
+plt.title("quantitative measure vs bmi")
 
-plt.xlabel("Temperature ºF")
-plt.ylabel("Chirps per Minute")
-plt.title("Cricket Chirps by Temperature")
-plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
-
+# show the plot
 plt.legend()
 plt.show()
