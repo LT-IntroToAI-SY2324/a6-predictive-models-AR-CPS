1e4a2c5e72b1e8ba1e6af08d410afe41f085d6a5
AR-CPS <aaramos4@cps.edu>
2023-12-04 11:34:44 -0600

    bjjern

--- part3-multivariable-linear-regression/example3_analyzing_car_data.py
+++ part3-multivariable-linear-regression/example3_analyzing_car_data.py
@@ -2,13 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
-x_1 = data["miles"]
+x_1 = data["miles(000)"]
 x_2 = data["age"]
 y = data["Price"]
 
 fig, graph = plt.subplots(2)
 graph[0].scatter(x_1, y)
-graph[0].set_xlabel("Total Miles")
+graph[0].set_xlabel("Total Miles(per 1000)")
 graph[0].set_ylabel("Price")
 
 graph[1].scatter(x_2, y)
